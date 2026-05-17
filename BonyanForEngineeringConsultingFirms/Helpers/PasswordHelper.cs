9e64494187774a75d45f4d7e06ad830b8118b960
youssef.emad <youssef.e.ghattas@gmail.com>
2026-05-17 03:23:14 +0300

    security is done :)

---
+++ BonyanForEngineeringConsultingFirms/Helpers/PasswordHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace BonyanForEngineeringConsultingFirms.Helpers
+{
+	public static class PasswordHelper
+	{
+		public static string HashMD5(string input)
+		{
+			using var md5 = MD5.Create();
+			var bytes = Encoding.UTF8.GetBytes(input);
+			var hash = md5.ComputeHash(bytes);
+			return string.Concat(hash.Select(b => b.ToString("x2")));
+		}
+	}
+}
